4de09782ca9ffe9383adb8d239394616c710be5b
sunny765 <37470709+sunny765@users.noreply.github.com>
2018-11-02 23:18:40 -0700

    Update PuBgGuRu.py

--- PuBgGuRu.py
+++ PuBgGuRu.py
@@ -13,17 +13,17 @@ from discord import Game, Embed, Color, Status, ChannelType
 
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
-client = Bot(description="PuBgGuRu Bot is best", command_prefix="pb!", pm_help = True)
+client = Bot(description="PuBgGuRu Bot is best", command_prefix="pg!", pm_help = True)
 client.remove_command('help')
 
 
 async def status_task():
     while True:
-        await client.change_presence(game=discord.Game(name='for pb!help'))
+        await client.change_presence(game=discord.Game(name='for pg!help'))
         await asyncio.sleep(5)
-        await client.change_presence(game=discord.Game(name='for pb!help'))
+        await client.change_presence(game=discord.Game(name='for pg!help'))
         await asyncio.sleep(5)
-        await client.change_presence(game=discord.Game(name='for pb!help'))
+        await client.change_presence(game=discord.Game(name='for pg!help'))
         await asyncio.sleep(5)
         
         
@@ -269,7 +269,7 @@ async def warn(ctx, userName: discord.User, *, message:str):
 @client.command(pass_context=True)
 async def ownerinfo(ctx):
     embed = discord.Embed(title="Information about owner", description="Bot Name- PuBg GuRu#3527", color=0x00ff00)
-    embed.set_footer(text="Copyright@UK Soft")
+    embed.set_footer(text="Copyright@PuBgGuRu")
     embed.set_author(name=" Bot Owner Name- |Sunny Singh|™✓#4856, PuBg GuRu#3527\nID:395535610548322326,435485523088506880")
     embed.add_field(name="Site-COMING SOON!!! ", value="THANKS!!!", inline=True)
     await client.say(embed=embed)
@@ -424,4 +424,344 @@ async def generalhelp(ctx):
 
 @client.command(pass_context=True)  
 @commands.has_permissions(kick_members=True)     
-async def kick(ctx,user:discor
+@client.command(pass_context=True)  
+@commands.has_permissions(kick_members=True)     
+async def kick(ctx,user:discord.Member):
+
+    if user.server_permissions.kick_members:
+        await client.say('**He is mod/admin and i am unable to kick him/her**')
+        return
+    
+    try:
+        await client.kick(user)
+        await client.say(user.name+' was kicked. Good bye '+user.name+'!')
+        await client.delete_message(ctx.message)
+
+    except discord.Forbidden:
+        await client.say('Permission denied.')
+        return
+
+@client.command(pass_context = True)
+@commands.has_permissions(manage_messages=True)  
+async def clear(ctx, number):
+ 
+    if ctx.message.author.server_permissions.manage_messages:
+         mgs = [] #Empty list to put all the messages in the log
+         number = int(number) #Converting the amount of messages to delete to an integer
+    async for x in client.logs_from(ctx.message.channel, limit = number+1):
+        mgs.append(x)            
+       
+    try:
+        await client.delete_messages(mgs)          
+        await client.say(str(number)+' messages deleted')
+     
+    except discord.Forbidden:
+        await client.say(embed=Forbidden)
+        return
+    except discord.HTTPException:
+        await client.say('clear failed.')
+        return         
+   
+ 
+    await client.delete_messages(mgs)      
+
+
+
+    	 		
+
+
+@client.command(pass_context=True)  
+@commands.has_permissions(ban_members=True)      
+async def ban(ctx,user:discord.Member):
+
+    if user.server_permissions.ban_members:
+        await client.say('**He is mod/admin and i am unable to ban him/her**')
+        return
+
+    try:
+        await client.ban(user)
+        await client.say(user.name+' was banned. Good bye '+user.name+'!')
+
+    except discord.Forbidden:
+
+        await client.say('Permission denied.')
+        return
+    except discord.HTTPException:
+        await client.say('ban failed.')
+        return		 
+
+
+
+@client.command(pass_context=True)  
+@commands.has_permissions(ban_members=True)     
+
+
+async def unban(ctx):
+    ban_list = await client.get_bans(ctx.message.server)
+
+    # Show banned users
+    await client.say("Ban list:\n{}".format("\n".join([user.name for user in ban_list])))
+
+    # Unban last banned user
+    if not ban_list:
+    	
+        await client.say('Ban list is empty.')
+        return
+    try:
+        await client.unban(ctx.message.server, ban_list[-1])
+        await client.say('Unbanned user: `{}`'.format(ban_list[-1].name))
+    except discord.Forbidden:
+        await client.say('Permission denied.')
+        return
+    except discord.HTTPException:
+        await client.say('unban failed.')
+        return		      	 		 		  
+  
+@client.command(pass_context = True)
+@commands.has_permissions(administrator=True)
+async def say(ctx, *, msg = None):
+    await client.delete_message(ctx.message)
+
+    if not msg: await client.say("Please specify a message to send")
+    else: await client.say(msg)
+    return
+
+@client.command(pass_context = True)
+@commands.has_permissions(kick_members=True)
+async def rules(ctx, *, msg = None):
+    await client.delete_message(ctx.message)
+
+    if not msg: await client.say("Please specify a user to warn")
+    else: await client.say(msg + ', Please Read Rules again and never break any one of them again otherwise i will mute/kick/ban you next time.')
+    return
+
+@client.command(pass_context = True)
+@commands.has_permissions(administrator=True) 
+async def bans(ctx):
+    '''Gets A List Of Users Who Are No Longer With us'''
+    x = await client.get_bans(ctx.message.server)
+    x = '\n'.join([y.name for y in x])
+    embed = discord.Embed(title = "List of The Banned Idiots", description = x, color = 0xFFFFF)
+    return await client.say(embed = embed)
+
+@client.command(pass_context=True)  
+@commands.has_permissions(kick_members=True)     
+
+async def serverinfo(ctx):
+    '''Displays Info About The Server!'''
+
+    server = ctx.message.server
+    roles = [x.name for x in server.role_hierarchy]
+    role_length = len(roles)
+
+    if role_length > 50: #Just in case there are too many roles...
+        roles = roles[:50]
+        roles.append('>>>> Displaying[50/%s] Roles'%len(roles))
+
+    roles = ', '.join(roles);
+    channelz = len(server.channels);
+    time = str(server.created_at); time = time.split(' '); time= time[0];
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    join = discord.Embed(description= '%s '%(str(server)),title = 'Server Name', color = discord.Color((r << 16) + (g << 8) + b));
+    join.set_thumbnail(url = server.icon_url);
+    join.add_field(name = '__Owner__', value = str(server.owner) + '\n' + server.owner.id);
+    join.add_field(name = '__ID__', value = str(server.id))
+    join.add_field(name = '__Member Count__', value = str(server.member_count));
+    join.add_field(name = '__Text/Voice Channels__', value = str(channelz));
+    join.add_field(name = '__Roles (%s)__'%str(role_length), value = roles);
+    join.set_footer(text ='Created: %s'%time);
+
+    return await client.say(embed = join);
+
+@client.command(pass_context = True)
+@commands.has_permissions(kick_members=True)
+async def norole(ctx, *, msg = None):
+    await client.delete_message(ctx.message)
+
+    if not msg: await client.say("Please specify a user to warn")
+    else: await client.say(msg + ', Please Do not ask for promotions check Rules again.')
+    return
+
+@client.command(pass_context = True)
+async def happybirthday(ctx, *, msg = None):
+    if not msg: await client.say("Please specify a user to wish")
+    else: await client.say('Happy birthday ' + msg + '\nhttps://asset.holidaycardsapp.com/assets/card/b_day399-22d0564f899cecd0375ba593a891e1b9.png')
+    return
+
+	
+@client.command(pass_context = True)
+@commands.has_permissions(kick_members=True)
+async def english(ctx, *, msg = None):
+    await client.delete_message(ctx.message)
+
+    if not msg: await client.say("Please specify a user to warn")
+    else: await client.say(msg + ', Please do not use language other than **English.**')
+    return
+
+@client.command(pass_context = True)
+async def brb(ctx, *, msg = None):
+    await client.delete_message(ctx.message)
+
+    if not msg: await client.say("Please specify a string")
+    else: await client.say('This user is brb for ' + msg)
+    return
+
+@client.command(pass_context = True)
+@commands.has_permissions(kick_members=True)
+async def welcomedbs(ctx, *, msg = None):
+    await client.delete_message(ctx.message)
+
+    if not msg: await client.say("Please specify a user to welcome")
+    else: await client.say('Welcome' + msg +  ', Please check <#474572305192845312> and never try to break any one of them')
+    return
+
+
+@client.command(pass_context = True) 
+
+async def htmltutorial(ctx, *, msg = None):
+    await client.delete_message(ctx.message)
+
+    if not msg: await client.say("Please specify a user")
+    else: await client.say('Welcome' + msg +  ', Please check http://uksoft.000webhostapp.com/Programming-Tutorials/index.html')
+    return
+   
+@client.command(pass_context = True)
+async def github(ctx, *, msg = None):
+    if not msg: await client.say("Please specify respo. ``Format- https://github.com/uksoftworld/DarkBot``")
+    else: await client.say('https://github.com/' + msg)
+    return
+
+@client.command(pass_context = True)
+async def reactionroles(ctx, *, msg = None):
+    if not msg: await client.say("Check this video to setup YAGPDB BOT- https://www.youtube.com/watch?v=icAqiw6txRQ")
+    else: await client.say('Check this video to setup YAGPDB BOT- https://www.youtube.com/watch?v=icAqiw6txRQ ' + msg)
+    return
+
+@client.command(pass_context = True)
+async def bottutorial(ctx, *, msg = None):
+    if not msg: await client.say("Tutorial not found or maybe you have mistyped it")
+    else: await client.say('https://github.com/uksoftworld/discord.py-tutorial/blob/master/' + msg + '.py')
+    return
+
+@client.command(pass_context = True)
+async def dyno(ctx, *, msg = None):
+    if not msg: await client.say("Command name not found or maybe you have mistyped it")
+    else: await client.say('https://github.com/uksoftworld/dynoCC/blob/master/' + msg)
+    return
+
+@client.command(pass_context=True)
+async def unverify(ctx):
+    await client.delete_message(ctx.message)
+    role = discord.utils.get(ctx.message.server.roles, name='Unverified')
+    await client.add_roles(ctx.message.author, role)
+    
+@client.command(pass_context=True)
+async def verify(ctx):
+    await client.delete_message(ctx.message)
+    role = discord.utils.get(ctx.message.server.roles, name='Verified')
+    await client.add_roles(ctx.message.author, role)
+    
+@client.command(pass_context=True)
+@commands.has_permissions(administrator=True)
+async def friend(ctx, user:discord.Member,):
+    await client.delete_message(ctx.message)
+    role = discord.utils.get(ctx.message.server.roles, name='Friend of Owner')
+    await client.add_roles(ctx.message.mentions[0], role)
+
+@client.command(pass_context = True)
+@commands.has_permissions(administrator=True)     
+async def makemod(ctx, user: discord.Member):
+    nickname = '♏' + user.name
+    await client.change_nickname(user, nickname=nickname)
+    role = discord.utils.get(ctx.message.server.roles, name='Moderator')
+    await client.add_roles(user, role)
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+    embed.set_author(name='Congratulations Message')
+    embed.add_field(name = '__Congratulations__',value ='**Congratulations for mod.Hope you will be more active here. Thanks for your help and support.**',inline = False)
+    embed.set_image(url = 'https://preview.ibb.co/i1izTz/ezgif_5_e20b665628.gif')
+    await client.send_message(user,embed=embed)
+    await client.delete_message(ctx.message)
+    
+@client.command(pass_context = True)
+@commands.has_permissions(administrator=True)     
+async def removemod(ctx, user: discord.Member):
+    nickname = user.name
+    await client.change_nickname(user, nickname=nickname)
+    role = discord.utils.get(ctx.message.server.roles, name='Moderator')
+    await client.remove_roles(user, role)
+    await client.delete_message(ctx.message)
+
+@client.command(pass_context = True)
+async def botwarncode(ctx):
+    await client.say('https://hastebin.com/ibogudoxot.py')
+    return
+
+@client.command(pass_context=True)
+async def guess(ctx, number):
+    try:
+        arg = random.randint(1, 10)
+    except ValueError:
+        await client.say("Invalid number")
+    else:
+        await client.say('The correct answer is ' + str(arg))
+
+@client.command(pass_context=True)
+@commands.has_permissions(kick_members=True) 
+async def roles(context):
+	"""Displays all of the roles with their ids"""
+	roles = context.message.server.roles
+	result = "The roles are "
+	for role in roles:
+		result += '``' + role.name + '``' + ": " + '``' + role.id + '``' + "\n "
+	await client.say(result)
+    
+@client.command(pass_context=True, aliases=['server'])
+@commands.has_permissions(kick_members=True)
+async def membercount(ctx, *args):
+    """
+    Shows stats and information about current guild.
+    ATTENTION: Please only use this on your own guilds or with explicit
+    permissions of the guilds administrators!
+    """
+    if ctx.message.channel.is_private:
+        await bot.delete_message(ctx.message)
+        return
+
+    g = ctx.message.server
+
+    gid = g.id
+    membs = str(len(g.members))
+    membs_on = str(len([m for m in g.members if not m.status == Status.offline]))
+    users = str(len([m for m in g.members if not m.bot]))
+    users_on = str(len([m for m in g.members if not m.bot and not m.status == Status.offline]))
+    bots = str(len([m for m in g.members if m.bot]))
+    bots_on = str(len([m for m in g.members if m.bot and not m.status == Status.offline]))
+    created = str(g.created_at)
+    
+    em = Embed(title="Membercount")
+    em.description =    "```\n" \
+                        "Members:   %s (%s)\n" \
+                        "  Users:   %s (%s)\n" \
+                        "  Bots:    %s (%s)\n" \
+                        "Created:   %s\n" \
+                        "```" % (membs, membs_on, users, users_on, bots, bots_on, created)
+
+    await client.send_message(ctx.message.channel, embed=em)
+    await client.delete_message(ctx.message)
+	
+@client.command(pass_context=True)
+@commands.has_permissions(administrator=True)
+async def embed(ctx, *args):
+    """
+    Sending embeded messages with color (and maby later title, footer and fields)
+    """
+    argstr = " ".join(args)
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    text = argstr
+    color = discord.Color((r << 16) + (g << 8) + b)
+    await client.send_message(ctx.message.channel, embed=Embed(color = color, description=text))
+    await client.delete_message(ctx.message)
+
+
+client.run(os.getenv('token'))
